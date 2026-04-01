858d3a6483468d104b50b1920c6bee84f33bebb8
Pasyah Vegananda <32693282+PVegananda@users.noreply.github.com>
2026-04-01 12:54:05 +0700

    Update settings.py
    agar bisa di akses lewat docker

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-+e&kicyjbklceo#^t9_p8)e(e074v2@-e9c)auby*=+tf=-w=n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
