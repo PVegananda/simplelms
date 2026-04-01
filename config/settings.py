05620ad7d87fc09c7134b44d220d6a7a792dd3ee
Pasyah Vegananda <32693282+PVegananda@users.noreply.github.com>
2026-04-01 13:08:05 +0700

    fix error database
    fix database belum masuk ke postgress sql

--- config/settings.py
+++ config/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
+        'NAME': 'lms_db',
+        'USER': 'lms_user',
+        'PASSWORD': 'lms_pass',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
