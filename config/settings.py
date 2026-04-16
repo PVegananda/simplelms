f5f467f890edc648d3ac5cc32e35e9d5a15abe34
Pasyah Vegananda <32693282+PVegananda@users.noreply.github.com>
2026-04-16 20:44:26 +0700

    step 1.3: tambah SilkyMiddleware ke MIDDLEWARE

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'silk.middleware.SilkyMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
