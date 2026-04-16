271b59022f5966c9adc0adffd7fe34ea82ad1a1c
Pasyah Vegananda <32693282+PVegananda@users.noreply.github.com>
2026-04-16 20:48:58 +0700

    buat course_list_baseline endpoint

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Course
 
-# Create your views here.
+
+def course_list_baseline(request):
+    """
+    Endpoint: /lab/course-list/baseline/
+    N+1 Problem: 1 query untuk courses + N query untuk teacher
+    """
+    courses = Course.objects.all()
+    data = []
+    
+    for course in courses:
+        data.append({
+            'id': course.id,
+            'name': course.name,
+            'teacher': course.teacher.username,
+            'price': course.price,
+        })
+    
+    return JsonResponse({'data': data})
